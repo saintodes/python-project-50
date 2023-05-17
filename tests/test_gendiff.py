99af0cb6c8f182c5bef228920fe65a65a94bb264
saintodes <odes36@gmail.com>
2023-05-17 19:57:30 +0700

    plain tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,17 +5,17 @@ from gendiff.gendiff import generate_diff
 from gendiff.formatters.abstraction import create_abstraction
 
 
-def test_stylish_plain():
-    json_dict1 = 'tests/fixtures/plain/file1.json'
-    json_dict2 = 'tests/fixtures/plain/file2.json'
-    plain_result = 'tests/fixtures/plain/plain_expected.txt'
+def test_stylish_flat():
+    json_dict1 = 'tests/fixtures/flat/file1.json'
+    json_dict2 = 'tests/fixtures/flat/file2.json'
+    flat_result = 'tests/fixtures/flat/expected_diff.txt'
 
-    assert generate_diff(json_dict1, json_dict2, 'stylish') == open(plain_result, 'r').read()
+    assert generate_diff(json_dict1, json_dict2, 'stylish') == open(flat_result, 'r').read()
 
-    yml_dict1 = 'tests/fixtures/plain/file1.yml'
-    yml_dict2 = 'tests/fixtures/plain/file2.yml'
+    yml_dict1 = 'tests/fixtures/flat/file1.yml'
+    yml_dict2 = 'tests/fixtures/flat/file2.yml'
 
-    assert generate_diff(yml_dict1, yml_dict2, 'stylish') == open(plain_result, 'r').read()
+    assert generate_diff(yml_dict1, yml_dict2, 'stylish') == open(flat_result, 'r').read()
 
 
 def test_stylish_nested():
@@ -30,28 +30,29 @@ def test_stylish_nested():
 
     assert generate_diff(yml_dict1, yml_dict2, 'stylish') == open(nested_result, 'r').read()
 
+def test_plain_nested():
+    json_dict1 = 'tests/fixtures/nested/file1.json'
+    json_dict2 = 'tests/fixtures/nested/file2.json'
+    nested_result = 'tests/fixtures/plain/plain_expected.txt'
 
-# def test_plain_abstraction():
-#     json_file1 = 'tests/fixtures/plain/file1.json'
-#     json_file2 = 'tests/fixtures/plain/file2.json'
+    assert generate_diff(json_dict1, json_dict2, 'plain') == open(nested_result, 'r').read()
+
+    yml_dict1 = 'tests/fixtures/nested/file1.yml'
+    yml_dict2 = 'tests/fixtures/nested/file2.yml'
+
+    assert generate_diff(yml_dict1, yml_dict2, 'plain') == open(nested_result, 'r').read()
+
+# def test_flat_abstraction():
+#     json_file1 = 'tests/fixtures/flat/file1.json'
+#     json_file2 = 'tests/fixtures/flat/file2.json'
 #     with open(json_file1, 'r') as f:
 #         dict1 = json.load(f)
 #     with open(json_file2, 'r') as f:
 #         dict2 = json.load(f)
-#     expected_result = 'tests/fixtures/plain/expected_plain_abstraction.txt'
+#     expected_result = 'tests/fixtures/flat/expected_flat_abstraction.txt'
 
 #     assert create_abstraction(dict1, dict2) == open(expected_result, 'r').read()
 
-
-# def test_nested_abstraction():
-#     json_file1 = 'tests/fixtures/nested/file1.json'
-#     json_file2 = 'tests/fixtures/nested/file2.json'
-#     with open(json_file1, 'r') as f:
-#         dict1 = json.load(f)
-#     with open(json_file2, 'r') as f:
-#         dict2 = json.load(f)
-#     expected_result = 'tests/fixtures/nested/expexted_nested_abstraction.txt'
-
-#     assert create_abstraction(dict1, dict2) == open(expected_result, 'r').read()
+    
 
 
