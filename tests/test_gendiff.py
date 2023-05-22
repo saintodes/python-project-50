e6f1e64edae2b4b9ec3bbb4fcf110a4d19c4a147
saintodes <odes36@gmail.com>
2023-05-22 07:41:06 +0700

    fix check 1 remark 3

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,8 +1,8 @@
 # import pytest
 # import json
-
+from gendiff.tree_diff import create_node, create_tree_diff
+from gendiff.formatters.constants import ADDED, REMOVED, CHANGED, UNCHANGED, NESTED
 from gendiff.gendiff import generate_diff
-# from gendiff.formatters.abstraction import create_abstraction
 
 
 def test_stylish_flat():
@@ -79,3 +79,64 @@ def test_json_nested():
         generate_diff(yml_dict1, yml_dict2, "json")
         == open(nested_result, "r").read()
     )
+
+
+def test_create_node():
+    key = "test"
+    value = "value"
+    status = "status"
+    expected = {key: {"value": value, "status": status}}
+    assert create_node(key, value, status) == expected
+
+
+def test_create_tree_diff_unchanged():
+    dict1 = {"key1": "value1", "key2": "value2"}
+    dict2 = {"key1": "value1", "key2": "value2"}
+    expected = {
+        "key1": {"value": "value1", "status": UNCHANGED},
+        "key2": {"value": "value2", "status": UNCHANGED},
+    }
+    assert create_tree_diff(dict1, dict2) == expected
+
+
+def test_create_tree_diff_added():
+    dict1 = {"key1": "value1"}
+    dict2 = {"key1": "value1", "key2": "value2"}
+    expected = {
+        "key1": {"value": "value1", "status": UNCHANGED},
+        "key2": {"value": "value2", "status": ADDED},
+    }
+    assert create_tree_diff(dict1, dict2) == expected
+
+
+def test_create_tree_diff_removed():
+    dict1 = {"key1": "value1", "key2": "value2"}
+    dict2 = {"key1": "value1"}
+    expected = {
+        "key1": {"value": "value1", "status": UNCHANGED},
+        "key2": {"value": "value2", "status": REMOVED},
+    }
+    assert create_tree_diff(dict1, dict2) == expected
+
+
+def test_create_tree_diff_changed():
+    dict1 = {"key1": "value1", "key2": "value2"}
+    dict2 = {"key1": "value1", "key2": "value3"}
+    expected = {
+        "key1": {"value": "value1", "status": UNCHANGED},
+        "key2": {"old_value": "value2", "new_value": "value3", "status": CHANGED},
+    }
+    assert create_tree_diff(dict1, dict2) == expected
+
+
+def test_create_tree_diff_nested():
+    dict1 = {"key1": {"subkey1": "value1"}, "key2": "value2"}
+    dict2 = {"key1": {"subkey1": "value1"}, "key2": "value3"}
+    expected = {
+        "key1": {
+            "value": {"subkey1": {"value": "value1", "status": UNCHANGED}},
+            "status": NESTED,
+        },
+        "key2": {"old_value": "value2", "new_value": "value3", "status": CHANGED},
+    }
+    assert create_tree_diff(dict1, dict2) == expected
