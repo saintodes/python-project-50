6ef642a8fc285c33bb06a26e125fd879a377fdb4
saintodes <odes36@gmail.com>
2023-07-13 16:47:28 +0700

    simplifying create_tree_diff func

--- gendiff/tree_diff.py
+++ gendiff/tree_diff.py
@@ -1,9 +1,43 @@
-from gendiff.formatters.constants import ADDED, REMOVED, \
-    CHANGED, UNCHANGED, NESTED
+from gendiff.formatters.constants import (
+    ADDED,
+    REMOVED,
+    CHANGED,
+    UNCHANGED,
+    NESTED,
+)
 
 
 def create_node(key, value, status):
     return {key: {"value": value, "status": status}}
+
+
+def handle_nested(dict1, dict2, key, result):
+    child = create_tree_diff(dict1.get(key), dict2.get(key))
+    result.update(create_node(key, child, NESTED))
+
+
+def handle_removed(dict1, key, result):
+    result.update(create_node(key, dict1.get(key), REMOVED))
+
+
+def handle_added(dict2, key, result):
+    result.update(create_node(key, dict2.get(key), ADDED))
+
+
+def handle_changed(dict1, dict2, key, result):
+    result.update(
+        {
+            key: {
+                "old_value": dict1.get(key),
+                "new_value": dict2.get(key),
+                "status": CHANGED,
+            }
+        }
+    )
+
+
+def handle_unchanged(dict1, key, result):
+    result.update(create_node(key, dict1.get(key), UNCHANGED))
 
 
 def create_tree_diff(dict1, dict2):
@@ -15,23 +49,14 @@ def create_tree_diff(dict1, dict2):
         value1 = dict1.get(key)
         value2 = dict2.get(key)
         if isinstance(value1, dict) and isinstance(value2, dict):
-            child = create_tree_diff(value1, value2)
-            result.update(create_node(key, child, NESTED))
+            handle_nested(dict1, dict2, key, result)
         elif key in removed_keys:
-            result.update(create_node(key, dict1.get(key), REMOVED))
+            handle_removed(dict1, key, result)
         elif key in added_keys:
-            result.update(create_node(key, dict2.get(key), ADDED))
+            handle_added(dict2, key, result)
         else:
             if value1 == value2:
-                result.update(create_node(key, value1, UNCHANGED))
+                handle_unchanged(dict1, key, result)
             else:
-                result.update(
-                    {
-                        key: {
-                            "old_value": value1,
-                            "new_value": value2,
-                            "status": CHANGED,
-                        }
-                    }
-                )
+                handle_changed(dict1, dict2, key, result)
     return result
